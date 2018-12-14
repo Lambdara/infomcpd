f341a5a98e4d0e054ae4aebe56fd61e3d222bca0
tomsmeding <tom.smeding@gmail.com>
2018-12-14 14:26:11 +0100

    impl: Automatically run test files

--- impl/Main.hs
+++ impl/Main.hs
@@ -1,26 +1,39 @@
 module Main where
 
 import Data.List
+import System.Directory
 import System.Environment
 import System.Exit
 
 import Parser
 
 
-usage :: String
-usage = intercalate "\n"
-    [ "Usage: stack run <file.sed>"
-    , "Interprets the given sed program." ]
-
-main :: IO ()
-main = do
-    args <- getArgs
-    source <- case args of
-        [fname] -> readFile fname
-        _ -> die usage
-
+runFile :: String -> IO ()
+runFile fname = do
+    source <- readFile fname
     prog <- case parseProgram "<stdin>" source of
         Left err -> die (show err)
         Right prog -> return prog
 
     print prog
+
+
+usage :: String
+usage = intercalate "\n"
+    [ "Usage:"
+    , "  stack run sed <file.sed>  -- Interpret a sed program"
+    , "  stack run test            -- Run all files in ../tests" ]
+
+main :: IO ()
+main = do
+    args <- getArgs
+
+    case args of
+        ["sed", fname] -> runFile fname
+
+        ["test"] -> do
+            let path = "../tests/"
+            fnames <- listDirectory path
+            mapM_ runFile $ map (path ++) fnames
+
+        _ -> die usage
