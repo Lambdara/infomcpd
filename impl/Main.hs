c6e9a046e9ff2629659be749cb0ac4a6eae93322
tomsmeding <tom.smeding@gmail.com>
2018-12-14 13:33:41 +0100

    impl: Process and "run" a sed file in the Main entry point

--- impl/Main.hs
+++ impl/Main.hs
@@ -1,5 +1,26 @@
 module Main where
 
+import Data.List
+import System.Environment
+import System.Exit
+
+import Parser
+
+
+usage :: String
+usage = intercalate "\n"
+    [ "Usage: stack run <file.sed>"
+    , "Interprets the given sed program." ]
 
 main :: IO ()
-main = putStrLn "hoi!"
+main = do
+    args <- getArgs
+    source <- case args of
+        [fname] -> readFile fname
+        _ -> die usage
+
+    prog <- case parseProgram "<stdin>" source of
+        Left err -> die (show err)
+        Right prog -> return prog
+
+    print prog
