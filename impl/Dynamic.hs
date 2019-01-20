5d99fbb85f542be0ed7b1c1d70b0848976b54f66
tomsmeding <tom.smeding@gmail.com>
2019-01-20 15:57:36 +0100

    Dynamic semantics up-to-date with LaTeX, untested!

--- impl/Dynamic.hs
+++ impl/Dynamic.hs
@@ -1,89 +1,229 @@
-module Dynamic where
+module Dynamic(run) where
+
+import Data.List
+
 import AST
 
-data Phase = NextCycle | ImplicitWrite | EndCycle | Exec | Final
-  deriving (Show, Eq)
 
-data State = State { phase :: Phase
-                   , program :: Program
-                   , inputLines :: [String]
-                   , output :: String
-                   , pattern :: String
-                   , hold :: String
-                   , appendQueue :: String
-                   , lineNumber :: Int
-                   , tFlag :: Bool
-                   , nFlag :: Bool
-                   , addrs :: [Int]
-                   } deriving (Show)
+data Machine
+    = NextCycle State
+    | EndCycle State
+    | Exec [DynCmd] IP State
+    | Find Label [DynCmd] IP State
+    | Match BaseAddr State Machine Machine
+    | EnableAddr BaseAddr [DynCmd] IP State
+    | Final String
+  deriving (Show)
 
--- Convert program to initial state of small-step semantics
-startSmallStep :: Program -> String -> State
-startSmallStep p i =
-  smallStep state
-  where state = State { phase = NextCycle
-                      , program = p
-                      , inputLines = lines i
-                      , output = ""
-                      , pattern = ""
-                      , hold = ""
-                      , appendQueue = ""
-                      , lineNumber = 0
-                      , tFlag = False
-                      , nFlag = False
-                      , addrs = [0]
-                      }
+data State = State
+        { sCode :: [DynCmd]
+        , sInputLines :: [String]
+        , sOutput :: String
+        , sPat :: String
+        , sHold :: String
+        , sAq :: String
+        , sLnnum :: Int
+        , sTflag :: Bool
+        , sNflag :: Bool
+        , sAddrs :: [[Int]]
+        }
+  deriving (Show)
 
--- Needs something for blocks
-cmd :: Program -> [Int] -> Maybe Cmd
-cmd (Program cmds) [x] = if length cmds > x
-                         then Just (cmds !! x)
-                         else Nothing
-cmd _ _ = error "Unimplemented in function cmd"
+data DownArrow = DownArrow
+  deriving (Show)
 
-incaddrs :: [Int] -> [Int]
-incaddrs [x] = [x + 1]
-incaddrs (x:xs) = x:(incaddrs xs)
-incaddrs _ = error "Unimplemented in function incaddrs"
+type DynCmd = Either DownArrow Cmd
 
-smallStep :: State -> State
-smallStep state
-  | phase state == NextCycle = if inputLines state == []
-                               then state { phase = Final
-                                          , output = output state ++ appendQueue state
-                                          }
-                               else smallStep state { phase = Exec
-                                                    , output = output state ++ appendQueue state
-                                                    , inputLines = (tail . inputLines) state
-                                                    , pattern = (head . inputLines) state
-                                                    , appendQueue = ""
-                                                    , lineNumber = lineNumber state + 1
-                                                    , addrs = [0]
-                                                    }
-  | phase state == Exec = smallStepExec state (cmd (program state) (addrs state))
-  | phase state == EndCycle = smallStep $
-                              if nFlag state
-                              then state { phase = NextCycle }
-                              else state { phase = NextCycle
-                                         , output = (output state) ++ (pattern state)
-                                         }
-  | phase state == Final = state
-  | otherwise = error "Unimplemented condition in smallStep"
+type IP = [Int]
 
-smallStepExec :: State -> Maybe Cmd -> State
-smallStepExec state Nothing = smallStep $ state { phase = EndCycle }
-smallStepExec state (Just (Cmd NoAddr fun)) = smallStep result
+-- Run a program according to the dynamic semantics
+run :: Program -> String -> Bool -> String
+run (Program cmds) input nflag = transitionLoop (NextCycle initState)
   where
-    result =
-      case fun of
-        Append string -> state { addrs = incaddrs (addrs state)
-                               , appendQueue = appendQueue state ++ string
-                               }
-        _ -> error "Unimplemented function in smallStepExec"
-smallStepExec _ (Just (Cmd _ _)) =
-  error "Unimplemented address specification in smallStepExec"
+    initState = State
+      { sCode = map Right cmds
+      , sInputLines = lines input
+      , sOutput = ""
+      , sPat = ""
+      , sHold = ""
+      , sAq = ""
+      , sLnnum = 0
+      , sTflag = False
+      , sNflag = nflag
+      , sAddrs = []
+      }
 
-testProg :: Program
-testProg = Program [
-  Cmd NoAddr (Append "hihi")
-  ]
+transitionLoop :: Machine -> String
+transitionLoop (Final str) = str
+transitionLoop machine = transitionLoop (smallStep machine)
+
+smallStep :: Machine -> Machine
+smallStep (NextCycle state)
+  | null (sInputLines state) = Final (sOutput state ++ sAq state )
+  | otherwise =
+      let (line : rest) = sInputLines state
+      in Exec (sCode state) [0]
+            state { sOutput = sOutput state ++ sAq state
+                  , sAq = ""
+                  , sInputLines = rest
+                  , sLnnum = sLnnum state + 1
+                  , sPat = line
+                  , sTflag = False
+                  }
+smallStep (EndCycle state) =
+    NextCycle (implWrite state)
+smallStep (Exec [] _ip state) = EndCycle state
+smallStep (Exec (Left DownArrow : code) (_n : m : ip) state) =
+    Exec code (m + 1 : ip) state
+smallStep (Exec (Left DownArrow : _code) _ip _state) =
+    error "Inconsistency: invalid IP in DownArrow"
+smallStep (Exec (Right cmd : code) ip state) =
+    execCmd cmd code ip state
+smallStep (Find label code ip state) =
+    execFind label code ip state
+smallStep (Match (ALine n) state machine1 machine2) =
+    if sLnnum state == n then machine1 else machine2
+smallStep (Match AEnd state machine1 machine2) =
+    if null (sInputLines state) then machine1 else machine2
+smallStep (EnableAddr (ALine n) code ip state)
+  | n <= sLnnum state = Exec code ip state
+smallStep (EnableAddr _ code ip state) =
+    Exec code ip state { sAddrs = ip : sAddrs state }
+smallStep (Final _state) = error "Should not execute Final machine"
+
+execCmd :: Cmd -> [DynCmd] -> IP -> State -> Machine
+execCmd (Cmd NoAddr fun) code ip state =
+    execFun fun code ip state
+execCmd (Cmd (Addr1 True addr) fun) code ip state =
+    Match addr state (Exec (toDynCmd fun : code) ip state)
+                     (Exec code (incrIP ip) state)
+execCmd (Cmd (Addr1 False addr) fun) code ip state =
+    Match addr state (Exec code (incrIP ip) state)
+                     (Exec (toDynCmd fun : code) ip state)
+execCmd (Cmd (Addr2 True addr1 addr2) fun) code ip state
+  | ip `notElem` sAddrs state =
+      Match addr1 state (EnableAddr addr2 (toDynCmd fun : code) ip state)
+                        (Exec code (incrIP ip) state)
+  | otherwise =
+      let l2 = sAddrs state \\ [ip]
+      in Match addr2 state
+               (Match addr1 state
+                      (Exec (toDynCmd fun : code) ip state)
+                      (Exec (toDynCmd fun : code) ip state { sAddrs = l2 }))
+               (Exec (toDynCmd fun : code) ip state)
+execCmd (Cmd (Addr2 False addr1 addr2) fun) code ip state
+  | ip `notElem` sAddrs state =
+      Match addr1 state (EnableAddr addr2 code (incrIP ip) state)
+                        (Exec (toDynCmd fun : code) ip state)
+  | otherwise =
+      let l2 = sAddrs state \\ [ip]
+      in Match addr2 state
+               (Match addr1 state
+                      (Exec code (incrIP ip) state)
+                      (Exec code (incrIP ip) state { sAddrs = l2 }))
+               (Exec code (incrIP ip) state)
+
+execFun :: Fun -> [DynCmd] -> IP -> State -> Machine
+execFun (Block cmds) code ip state =
+    Exec (map Right cmds ++ [Left DownArrow] ++ code) (0 : ip) state
+execFun (Append text) code ip state =
+    Exec code (incrIP ip) state { sAq = sAq state ++ text ++ "\n" }
+execFun (Branch (Just label)) _code _ip state =
+    Find label (sCode state) [0] state
+execFun (Branch Nothing) _code _ip state =
+    EndCycle state
+execFun Delete _code _ip state =
+    NextCycle state
+execFun DeleteNL _code _ip state
+  | '\n' `notElem` sPat state = EndCycle state
+  | otherwise =
+      let (_line, rest) = splitLine (sPat state)
+      in Exec (sCode state) [0] state { sPat = rest }
+execFun Get code ip state =
+    Exec code (incrIP ip) state { sPat = sHold state }
+execFun GetAppend code ip state =
+    Exec code (incrIP ip) state { sPat = sPat state ++ "\n" ++ sHold state }
+execFun Hold code ip state =
+    Exec code (incrIP ip) state { sHold = sPat state }
+execFun HoldAppend code ip state =
+    Exec code (incrIP ip) state { sHold = sHold state ++ "\n" ++ sPat state }
+execFun (Insert text) code ip state =
+    Exec code (incrIP ip) state { sOutput = sOutput state ++ text ++ "\n" }
+execFun Next code ip state
+  | null (sInputLines state) = EndCycle state
+  | otherwise =
+      let state' = implWrite state
+          (line : rest) = sInputLines state
+      in Exec code (incrIP ip)
+              state' { sPat = line
+                     , sInputLines = rest
+                     , sLnnum = sLnnum state' + 1
+                     , sTflag = False
+                     }
+execFun NextAppend code ip state
+  | null (sInputLines state) = Final (sOutput state)
+  | otherwise =
+      let (line : rest) = sInputLines state
+      in Exec code (incrIP ip)
+              state { sPat = sPat state ++ "\n" ++ line
+                    , sInputLines = rest
+                    , sLnnum = sLnnum state + 1
+                    , sTflag = False
+                    }
+execFun Print code ip state =
+    Exec code (incrIP ip) state { sOutput = sOutput state ++ sPat state ++ "\n" }
+execFun PrintNL code ip state
+  | null (sPat state) = Exec code (incrIP ip) state { sOutput = sOutput state ++ "\n" }
+  | otherwise =
+      let (line, rest) = splitLine (sPat state)
+      in Exec code (incrIP ip)
+              state { sOutput = sOutput state ++ line ++ "\n"
+                    , sPat = rest
+                    }
+execFun Quit _code _ip state =
+    Final (sOutput (implWrite state))
+execFun (To (Just label)) code ip state
+  | sTflag state = Find label (sCode state) [0] state { sTflag = False }
+  | otherwise = Exec code (incrIP ip) state
+execFun (To Nothing) code ip state
+  | sTflag state = EndCycle state { sTflag = False }
+  | otherwise = Exec code (incrIP ip) state
+execFun Exchange code ip state =
+    Exec code (incrIP ip)
+         state { sPat = sHold state
+               , sHold = sPat state
+               }
+execFun (Label _label) code ip state =
+    Exec code (incrIP ip) state
+execFun LineNum code ip state =
+    Exec code (incrIP ip)
+         state { sOutput = sOutput state ++ show (sLnnum state) ++ "\n" }
+
+execFind :: Label -> [DynCmd] -> IP -> State -> Machine
+execFind _label [] _ip _state =
+    error "Inconsistency: label not found in Find"
+execFind label (Right (Cmd _ (Label label')) : code) ip state
+  | label == label' = Exec code (incrIP ip) state
+execFind label (Right (Cmd _ (Block cmds)) : code) ip state =
+    Find label (map Right cmds ++ [Left DownArrow] ++ code) (0 : ip) state
+execFind label (Left DownArrow : code) (_n : m : ip) state =
+    Find label code (m + 1 : ip) state
+execFind _label (Left DownArrow : _code) _ip _state =
+    error "Inconsistency: invalid IP in Find DownArrow"
+execFind label (_ : code) ip state =
+    Find label code (incrIP ip) state
+
+implWrite :: State -> State
+implWrite state
+  | sNflag state = state
+  | otherwise = state { sOutput = sOutput state ++ sPat state ++ "\n" }
+
+splitLine :: String -> (String, String)
+splitLine = span (/= '\n')
+
+incrIP :: IP -> IP
+incrIP (n : ip) = (n + 1 : ip)
+incrIP _ = undefined
+
+toDynCmd :: Fun -> DynCmd
+toDynCmd = Right . Cmd NoAddr
--- impl/Main.hs
+++ impl/Main.hs
@@ -7,35 +7,40 @@ import System.Exit
 
 import Parser
 import qualified Static as Static
+import qualified Dynamic as Dynamic
 
 
-runFile :: String -> IO ()
-runFile fname = do
+runFile :: String -> Bool -> IO ()
+runFile fname nflag = do
     source <- readFile fname
-    prog <- case parseProgram "<stdin>" source of
+    prog <- case parseProgram fname source of
         Left err -> die (show err)
         Right prog -> return prog
 
     print prog
     print $ Static.ok prog
+    input <- getContents
+    print $ Dynamic.run prog input nflag
 
 
 usage :: String
 usage = intercalate "\n"
     [ "Usage:"
-    , "  stack run sed <file.sed>  -- Interpret a sed program"
-    , "  stack run test            -- Run all files in ../tests" ]
+    , "  stack run sed [-n] <file.sed>  -- Interpret a sed program"
+    , "  stack run test                 -- Run all files in ../tests" ]
 
 main :: IO ()
 main = do
     args <- getArgs
 
     case args of
-        ["sed", fname] -> runFile fname
+        ["sed", fname] -> runFile fname False
+        ["sed", "-n", fname] -> runFile fname True
 
         ["test"] -> do
             let path = "tests/"
-            fnames <- listDirectory path
-            sequence_ $ intersperse (putStrLn "") $ map runFile $ map (path ++) fnames
+            fnames <- map (path ++) <$> listDirectory path
+            let runner fname = runFile fname False
+            sequence_ $ intersperse (putStrLn "") $ map runner fnames
 
         _ -> die usage
