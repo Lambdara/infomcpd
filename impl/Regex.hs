9098f500babe0cf28f8eaa46d462bd04cc4a4894
Uwila <uwila@runbox.com>
2019-01-25 13:39:59 +0100

    Implement full regex

--- impl/Regex.hs
+++ impl/Regex.hs
@@ -6,7 +6,7 @@ import Text.Parsec
 
 -- This is here just for debug purposes. If this is still here in the pull request then you should not merge xd
 testRegex :: Regex
-Right testRegex = parse (pRegexTill ' ') "" "a* "
+Right testRegex = parse (pRegexTill ' ') "" "^\\(a*\\)b\\1 "
 
 regex :: Regex -> String -> Maybe (String, String, [String])
 regex reg str =
@@ -20,9 +20,9 @@ regtry :: Regex -> String -> String -> [String] -> Maybe (String, String, String
 regtry reg str pre groups =
   case result of
     Just (post, match, groups') -> Just (pre, post, match, groups')
-    Nothing -> result'
-      where result' = regtry reg str' (pre ++ [c]) groups
-            (c:str') = str
+    Nothing -> case str of
+      (c:str') -> regtry reg str' (pre ++ [c]) groups
+      "" -> Nothing
   where result = r reg str (pre == "") groups
 
 regpp :: Regex -> Int -> (Regex, Int)
@@ -42,6 +42,67 @@ r (RegConcat ((RegChar c):rs)) (c':cs) _ groups =
     (post,match,groups') <- r (RegConcat rs) cs False groups
     return (post, (c:match), groups')
   else Nothing
+r (RegConcat ((RegClass polarity rcis):rs)) (c:cs) _ groups =
+  if rcicheck rcis c == polarity
+  then do
+    (post, match, groups') <- r (RegConcat rs) cs False groups
+    return (post, (c:match), groups')
+  else
+    Nothing
+  where
+    -- This checks whether the character matches any of the RegClassItems
+    rcicheck [] _ = False
+    rcicheck ((RCChar c1):rcis') ch =
+      c1 == ch || rcicheck rcis' ch
+    rcicheck ((RCRange c1 c2):rcis') ch =
+      ch `elem` [c1 .. c2] || rcicheck rcis' ch
+r (RegConcat (RegBackref n:rs)) s begin groups =
+  do
+    (post,match,groups') <- r (RegConcat rs) s2 (begin && s1 == []) groups
+    return (post,(s1 ++ match),groups')
+  where
+    s1 = groups !! (n-1)
+    s2 = drop (length s1) s
+r (RegConcat (RegAnchorLeft:rs)) s begin groups =
+  if begin
+  then do
+    result <- r (RegConcat rs) s begin groups
+    return result
+  else Nothing
+r (RegConcat (RegAnchorRight:rs)) s begin groups =
+  if s == ""
+  then do
+    result <- r (RegConcat rs) s begin groups
+    return result
+  else Nothing
+r (RegConcat ((RegGroup r1 n):rs)) s begin groups =
+  do
+    (post1, match1, groups1) <- r r1 s begin groups
+    (post, match2, groups') <- r (RegConcat rs) post1 (begin && match1 == "") (replace groups1 (n-1) match1)
+    return (post, match1 ++ match2, groups')
+  where
+    replace (_:list) 0 content = content:list
+    replace (x:list) index content = x:(replace list (index - 1) content)
+    replace _ _ _ = error "No match in r.replace"
+r (RegConcat ((RegRep _ 0 (Just 0)):rs)) s begin groups =
+  r (RegConcat rs) s begin groups
+r (RegConcat ((RegRep r1 0 (Just m)):rs)) s begin groups =
+  if m > 0
+  then case result of
+    Just x -> Just x
+    Nothing -> r (RegConcat rs) s begin groups
+  else Nothing
+  where result = r (RegConcat (r1:(RegRep r1 0 (Just (m-1))):rs)) s begin groups
+r (RegConcat ((RegRep r1 n (Just m)):rs)) s begin groups =
+  if n > 0
+  then r (RegConcat (r1:(RegRep r1 (n-1) (Just (m-1))):rs)) s begin groups
+  else Nothing
+r (RegConcat ((RegRep r1 0 Nothing):rs)) s begin groups =
+  r (RegConcat ((RegStar r1):rs)) s begin groups
+r (RegConcat ((RegRep r1 n Nothing):rs)) s begin groups =
+  if n > 0
+  then r (RegConcat (r1:(RegRep r1 (n-1) Nothing):rs)) s begin groups
+  else Nothing
 r (RegConcat ((RegStar r1):rs)) s begin groups =
   if result /= Nothing
   then result
@@ -49,4 +110,4 @@ r (RegConcat ((RegStar r1):rs)) s begin groups =
   where
     result = r (RegConcat (r1:(RegStar r1):rs)) s begin groups
     result' = r (RegConcat rs) s begin groups
-r _ _ _ _ = error "unimplemented r"
+r _ _ _ _ = Nothing
